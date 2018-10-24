a703633dc1462e345d1c52baebfbd9fd66b958a1
Nejo <andresjoelv@gmail.com>
2018-10-24 16:03:56 -0400

    timer left

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -44,10 +44,21 @@ $(document).ready(function() {
     var answersVar;
 
     var slides;
+    var answerSlide;
     let currSlide = 0;
     let questionNumber = 0;
     // isCorrect
-    
+
+    var correctBoolean;
+    var correctAnswerString = "";
+
+    var correctAnsLetter = "";
+
+    var labelText;
+    var label;
+    var result;
+
+    let index = 0;
 
     $('#btn-start').on('click', function(){
 
@@ -58,7 +69,12 @@ $(document).ready(function() {
         $('#btn-start').css('display', 'none');
 
         // Use setInterval to start the count here and set the clock to running.
-        //intervalId = setInterval(count, 1000);
+        // if (!clockRunning) {
+        //     intervalId = setInterval(count, 1000);
+        //     clockRunning = true;
+        //   }
+        // count();
+        
         
         var $gameDiv = $('#quiz');
 
@@ -73,50 +89,79 @@ $(document).ready(function() {
             questionArr.push(`<div class="slide">
                             <div class="question">${currQuestion.question}</div>
                             <div class="answers"><ul>${answers.join("")}</ul></div></div>`);
+            questionArr.push(`<div class="slide result" >
+                            <div id="answer-slide-label-${questionIndex+1}"></div>
+                            <div id="answer-result-${questionIndex+1}"></div></div>`);
         });
         
         $gameDiv.html(questionArr.join(""));
 
         slides = document.querySelectorAll(".slide");
         pagination(0);
+
+        
         
     });
     
 
     var pagination = function(n){
+        //console.log("10s");
         slides[currSlide].classList.remove('active-slide');
         slides[n].classList.add('active-slide');
         currSlide = n;
     }
 
-    var showResult = function(letter){
-        console.log("correct answer is " + letter);
+    var showResult = function(){
+        
+        index++;
+
+        correctAnsLetter = myQuestions[index-1].correctAns;
+        correctAnswerString = myQuestions[index-1].answers[correctAnsLetter];
+
+        labelText = "The correct answer was:";
+        label = $(`#answer-slide-label-${index}`);
+        result = $(`#answer-result-${index}`);
+
+        if(correctBoolean)
+            label.text("Correct");
+        else{
+            label.text("Oops!");
+            result.text(labelText + " " + correctAnswerString);
+        }
+    }
+
+    var next = function(){
+        pagination(currSlide + 1);
+        showResult();
+        setTimeout(function(){pagination(currSlide + 1)}, 2000);
     }
 
     var isCorrect = function(){
         // code here
-        var questionNumber = parseInt($(this).attr("data-number"));
-        var correctAnsLetter = myQuestions[questionNumber].correctAns;
-        var correctAnswerString = myQuestions[questionNumber].answers[correctAnsLetter];
+        clearInterval(intervalId);
+        
+        //var questionNumber = parseInt($(this).attr("data-number"));
+        correctAnsLetter = myQuestions[index].correctAns;
+        correctAnswerString = myQuestions[index].answers[correctAnsLetter];
         var userChoice = $(this).attr("data-answer");
+        correctBoolean = false;
 
-        if ((userChoice === correctAnsLetter) ? pagination(currSlide + 1) : showResult(correctAnswerString));
+        if (userChoice === correctAnsLetter) correctBoolean = true;
+        next();
+        
     }
 
     // Generic function for displaying the movieInfo
     $(document).on("click", ".answer", isCorrect);
 
+    function reset(){
+        timeRemaining = 5;
+    }
     
-
     
-
     function count() {
         //  TODO: increment time by 1, remember we cant use "this" here.
         
-    
-        //  TODO: Get the current time, pass that into the stopwatch.timeConverter function,
-        //        and save the result in a variable.
-        //var time = stopwatch.timeConverter(stopwatch.time);
     
         //  TODO: Use the variable you just created to show the converted time in the "display" div.
         $timeRemainingDiv = $('#time-remaining');
@@ -125,8 +170,11 @@ $(document).ready(function() {
         // display rule #1
         $timeRemainingDiv.html($timeRmn);
         timeRemaining--;
-        if(timeRemaining == -1)
+        if(timeRemaining == -1){ // runs out of time
             clearInterval(intervalId);
+            next();
+            reset();
+        }
     
     }
 });
