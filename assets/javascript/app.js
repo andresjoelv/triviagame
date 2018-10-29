76b85028bcaa79405561ed2be862a6618fa3599a
Nejo <andresjoelv@gmail.com>
2018-10-29 03:36:08 -0400

    final commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,211 +1,298 @@
 $(document).ready(function() {
+
+    $("#time-remaining").hide();
+
+    //  Variable that will hold our setInterval that runs the stopwatch
+    var intervalId = "";
+
+    // prevents the clock from being sped up unnecessarily
+    var clockRunning = false;
+    var countdown = 20;
+
+    var slides;
+    let currSlide = 0;
+
+    var correctAnswerString = "";
+    var correctAnsLetter = "";
+
+    var labelText;
+
+    let delay = 3000;
+
+    let message = "";
+
+    var wins = 0;
+    var losses = 0;
+    var unAns = 0;
+
     // create game as object
     // create an object containing arrays for each possible question
 
     var myQuestions = [
         {
-            question : "What was the first full length CGI movie",
+            question : "How many times has Ross been divorced?",
             answers : {
-                a : "A Bug's Life",
-                b : "Monsters Inc.",
-                c : "Toy Story",
-                d : "The Lion King"
+                a : "5",
+                b : "2",
+                c : "1",
+                d : "3"
             },
-            correctAns : "c"
+            correctAns : "d"
         },
         {
-            question : 'Which one of this is NOT a name of one of the Spice Girls',
+            question : 'How many types of towels does Monica have?',
             answers : {
-                a : "Sporty Spice",
-                b : "Fred Spice",
-                c : "Scary Spice",
-                d : "Posh Spice"
+                a : "3",
+                b : "11",
+                c : "8",
+                d : "6"
             },
             correctAns : "b"
         },
         {
-            question : "Which NBA team won the most titles in the 90s",
+            question : "What does Joey love to eat?",
             answers : {
-                a : "New York Knicks",
-                b : "Portland Traiblazers",
-                c : "Los Angeles Lakers",
-                d : "Chicago Bulls"
+                a : "Apples",
+                b : "Sandwiches",
+                c : "Fish",
+                d : "Cake"
             },
-            correctAns : "d"
+            correctAns : "b"
         },
+        {
+            question : "Who is actually a chef?",
+            answers : {
+                a : "Monica",
+                b : "Chandler",
+                c : "Ross",
+                d : "Rachel"
+            },
+            correctAns : "a"
+        },
+        {
+            question : "Who stole Monica's thunder after she got engaged?",
+            answers : {
+                a : "Rachel",
+                b : "Phoebe",
+                c : "Emily",
+                d : "Carol"
+            },
+            correctAns : "a"
+        },
+        {
+            question : "Who hates Thanksgiving?",
+            answers : {
+                a : "Ross",
+                b : "Rachel",
+                c : "Chandler",
+                d : "Joey"
+            },
+            correctAns : "c"
+        },
+        {
+            question : "Who thinks they're always the last to find out everything?",
+            answers : {
+                a : "Ross",
+                b : "Phoebe",
+                c : "Monica",
+                d : "Chandler"
+            },
+            correctAns : "b"
+        }
     ];
 
-    //  Variable that will hold our setInterval that runs the stopwatch
-    var intervalId;
-    // prevents the clock from being sped up unnecessarily
-    var clockRunning = false;
-
-    var nn = 20;
-    var timeRemaining = nn;
-    var answersVar;
-
-    var slides;
-    var answerSlide;
-    let currSlide = 0;
-    let questionNumber = 0;
-    // isCorrect
-
-    var correctBoolean;
-    var correctAnswerString = "";
-
-    var correctAnsLetter = "";
-
-    var labelText;
-    var label;
-    var result;
-
-    let index = 0;
-
-    let delay = 2000;
-
-    let message = "";
-    var isOutOfTime = false;
-
     $('#btn-start').on('click', function(){
+       startGame();
+    });
 
-        $(".quiz-container").css("display", "block");
-        
+    function startGame(){
+
+        currSlide = 0;
+        index = 0;
         var questionArr = [];
-        // erase button
-        $('#btn-start').css('display', 'none');
+        [wins, losses, unAns] = [0, 0, 0];
+        clearInterval(intervalId);
 
-        start();
+        // show timer div
+        $("#time-remaining").show();
+
+        // show game div
+        $(".quiz-container").show();
+        
+        // hide button
+        $('#btn-start').hide();
         
         var $gameDiv = $('#quiz');
+
+        myQuestions.sort( () => Math.random() - 0.5);
+        
 
         myQuestions.forEach((currQuestion, questionIndex) => { // value, index!!!
             const answers = [];
             for(letter in currQuestion.answers){
 
                  answers.push(
-                            `<li><a class="answer" data-number="${questionIndex}" data-answer="${letter}">${currQuestion.answers[letter]}</a></li>`
+                            `<li class="answer" data-number="${questionIndex}" data-answer="${letter}"><a>${currQuestion.answers[letter]}</a></li>`
                         );
             }
-            questionArr.push(`<div class="slide">
+            questionArr.push(`
+                            <div class="slide">
                             <div class="question">${currQuestion.question}</div>
                             <div class="answers"><ul>${answers.join("")}</ul></div></div>`);
-            questionArr.push(`<div class="slide result" >
-                            <div id="answer-slide-label-${questionIndex+1}"></div>
-                            <div id="answer-result-${questionIndex+1}"></div></div>`);
         });
         
         $gameDiv.html(questionArr.join(""));
 
         slides = document.querySelectorAll(".slide");
-        pagination(0);
-
-        
-        
-    });
-    
-
-    var pagination = function(n){
-        //console.log("10s");
-        slides[currSlide].classList.remove('active-slide');
-        slides[n].classList.add('active-slide');
-        currSlide = n;
+        nextQuestion(0);
     }
 
-    var showResult = function(){
+    /**
+     * nextQuestion
+     */
+    var nextQuestion = function(n){
         
-        index++;
+        countdown = 14;
+        $("#countdown").removeClass("last-seconds");
+        $("#countdown").removeClass("beating");
+        $("#countdown").text(countdown);
 
-        correctAnsLetter = myQuestions[index-1].correctAns;
-        correctAnswerString = myQuestions[index-1].answers[correctAnsLetter];
-
-        labelText = "The correct answer was:";
-        label = $(`#answer-slide-label-${index}`);
-        result = $(`#answer-result-${index}`);
-
-        if(correctBoolean)
-            label.text("Correct");
-        else if(!isOutOfTime) {
-            message = "Oops!";
-            label.text(message);
-            result.text(labelText + " " + correctAnswerString);
-        }
-        else {
-            label.text("Out of Time!");
-            result.text(labelText + " " + correctAnswerString);
-        }
-
-        correctBoolean = false;
-    }
-
-    var next = function(){
-        pagination(currSlide + 1);
-        showResult();
-        setTimeout(function(){pagination(currSlide + 1)}, delay);
-    }
-
-    var isCorrect = function(){
-        stop();
-        reset();
-        
-        // code here
-        
-        //var questionNumber = parseInt($(this).attr("data-number"));
-        correctAnsLetter = myQuestions[index].correctAns;
-        correctAnswerString = myQuestions[index].answers[correctAnsLetter];
-        var userChoice = $(this).attr("data-answer");
-        correctBoolean = false;
-
-        if (userChoice === correctAnsLetter) correctBoolean = true;
-        next();
-        setTimeout(start, delay);
-        
-    }
-
-    // Generic function for displaying the movieInfo
-    $(document).on("click", ".answer", isCorrect);
-
-    function start(){
-        
-        // Use setInterval to start the count here and set the clock to running.
-        if (!clockRunning) {
+        // to prevent timer speed up
+        if(!clockRunning){
             count();
             intervalId = setInterval(count, 1000);
-            clockRunning = true;
         }
+        
+        // slide to next question if there is any
+        if(n !== slides.length){
+            slides[currSlide].classList.remove('active-slide');
+            slides[n].classList.add('active-slide');
+            currSlide = n;
+        }
+        else {
+            clearInterval(intervalId);
+            var $gameDiv = $('#quiz');
+            $gameDiv.html(`<h2>All Done here is how you did!</h2>
+                           <p>Correct Answers: ${wins}</p>
+                           <p>Incorrect Answers: ${losses}</p>
+                           <p>Unanswered: ${unAns}</p>
+                           <a class="button">Start Over?</a>`);
+        }
+        
     }
-    
-    function stop(){
-        clearInterval(intervalId);
-        clockRunning = false;
-    }
-    
-    function reset(){
-        timeRemaining = nn;
-    }
+
     function count() {
-        //  TODO: increment time by 1, remember we cant use "this" here.
-        
-    
-        //  TODO: Use the variable you just created to show the converted time in the "display" div.
-        $timeRemainingDiv = $('#time-remaining');
-        $timeRmn = $(`<h3>Time Remaining: ${timeRemaining} seconds</h3>`);
-        
-        // display rule #1
-        $timeRemainingDiv.html($timeRmn);
-        timeRemaining--;
-        
-    
-    }
-
-    function checktime(){
-        if(timeRemaining == -1){ // runs out of time
-            isOutOfTime = true;
-            reset();
-            stop();
-            next();
-            setTimeout(start, delay);
+        var hidden = $("#help");
+        if(countdown > -1 && currSlide < Object.keys(myQuestions).length){
+            $('#countdown').text(countdown);
+            
+            countdown--;
+            if(countdown === 9){
+                hidden.animate({"left":"0px"}, "slow").removeClass('visible');
+            }
+            if(countdown === 4){
+                $('#countdown').addClass('last-seconds');
+                $('#countdown').addClass('beating');
+            }
         }
+        else if(countdown === -1){ // === 0
+            
+            hidden.animate({"left":"-360px"}, "slow").addClass('visible');
+
+            unAns++;
+            clearInterval(intervalId);
+
+            var resultsDiv = $("#result");
+            resultsDiv.empty();
+
+            message = "<h2>Out of Time!</h2>";
+            resultsDiv.append(message);
+            labelText = `<p>The correct answer was: ${correctAnswerString}</p>`;
+            resultsDiv.append(labelText);
+            var gif = $('<img>');
+            gif.attr('src', "https://media.giphy.com/media/yoJC2oBVm4Dqhuidzy/giphy.gif");
+            resultsDiv.append(gif);
+
+            // inmediate result div
+            $("#quiz").hide();
+            resultsDiv.show();
+            setTimeout(quickSlide, delay);
+        }
+        
     }
 
-    setInterval(checktime,1000);
+    /**
+     * showResult
+     */
+    var showResult = function(){
+
+        var hidden = $("#help");
+        hidden.animate({"left":"-360px"}, "slow").addClass('visible');
+        
+        correctAnsLetter = myQuestions[currSlide].correctAns;
+        correctAnswerString = myQuestions[currSlide].answers[correctAnsLetter];
+
+        var resultsDiv = $("#result");
+        resultsDiv.empty();
+        var gif = $('<img>');
+
+        // is correct
+        if($(this).attr("data-answer") === correctAnsLetter){
+            wins++;
+            clearInterval(intervalId);
+            
+            message = "<h2>Correct!</h2>";
+            resultsDiv.append(message);
+            gif.attr('src', "https://media.giphy.com/media/lI6nHr5hWXlu0/giphy.gif");
+            resultsDiv.append(gif);
+
+            // inmediate result div
+            $("#quiz").hide();
+            resultsDiv.show();
+            setTimeout(quickSlide, delay);
+        }
+        // is not correct
+        else if($(this).attr("data-answer") != correctAnsLetter) { 
+            losses++;
+            clearInterval(intervalId);
+            
+            message = "<h2>Nope!</h2>";
+            resultsDiv.append(message);
+            labelText = `<p>The correct answer was: ${correctAnswerString}</p>`;
+            resultsDiv.append(labelText);
+            gif.attr('src', "https://media.giphy.com/media/rVouyGkIQYsnu/giphy.gif");
+            resultsDiv.append(gif);
+
+            // inmediate result div
+            $("#quiz").hide();
+            resultsDiv.show();
+            setTimeout(quickSlide, delay);
+        }
+
+    }
+
+    // method to remove previous question results and options
+    var quickSlide = function(){
+
+        // hide results
+        $("#result").hide()
+        $("#quiz").show();
+        
+        // begin next question
+        nextQuestion(currSlide+1);
+        
+    }
+
+    var hint = function(){
+        // a
+        //correctAnsLetter = myQuestions[currSlide].correctAns;
+        //var test = $(".button").data("data-number", correctAnsLetter).data();
+        
+    }
+
+   
+    // Generic function for displaying the movieInfo
+    $(document).on("click", ".answer", showResult);
+    $(document).on("click", ".button", startGame);
+        
 });
